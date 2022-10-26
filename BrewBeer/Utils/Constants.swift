67933ca0e4ff7cdcce33466fa090bcbdce7e09ff
daneccc <danielecavalcantecf@gmail.com>
2022-10-26 10:43:25 -0300

    fix base url

--- BrewBeer/Utils/Constants.swift
+++ BrewBeer/Utils/Constants.swift
@@ -10,8 +10,10 @@ import Foundation
 struct Constants {
     
     struct URLs {
-        static let allStyles = URL(string: "https://rustybeer.herokuapp.com/styles")!
-        static let allHops = URL(string: "https://rustybeer.herokuapp.com/hops")!
-        static let allYeasts = URL(string: "https://rustybeer.herokuapp.com/yeasts")!
+        static let baseURL = "https://rustybeer.herokuapp.com"
+        
+        static let allStyles = URL(string: "\(baseURL)/styles")!
+        static let allHops = URL(string: "\(baseURL)/hops")!
+        static let allYeasts = URL(string: "\(baseURL)/yeasts")!
     }
 }
